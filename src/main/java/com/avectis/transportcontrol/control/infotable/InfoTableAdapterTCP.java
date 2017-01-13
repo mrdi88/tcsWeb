376b8973a2c50e423b5260d665869c132b08c330
Ivan <Ivan@Ivan-P>
2017-01-13 09:21:27 +0200

    Multistring infotable tested & finished

--- src/main/java/com/avectis/transportcontrol/control/infotable/InfoTableAdapterTCP.java
+++ src/main/java/com/avectis/transportcontrol/control/infotable/InfoTableAdapterTCP.java
@@ -17,7 +17,7 @@ import java.util.Random;
  * @author Ivan
  */
 public class InfoTableAdapterTCP implements InfoTableAdapter {
-    
+ 
     private String ipAddr;
     private int port;
     private int InfoTableAddress;
@@ -356,4 +356,3 @@ public class InfoTableAdapterTCP implements InfoTableAdapter {
       (byte)233, (byte)183,  85,  11, (byte)136, (byte)214,  52, 106,  43, 117, (byte)151, (byte)201,  74,  20, (byte)246, (byte)168,
       116,  42, (byte)200, (byte)150,  21,  75, (byte)169, (byte)247, (byte)182, (byte)232,  10,  84, (byte)215, (byte)137, 107,  53};
 }
-
